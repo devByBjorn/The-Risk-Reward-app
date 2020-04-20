84c0a1e05aa1b722841284f230b270c0c074ec23
devByBjorn <devbybjorn@gmail.com>
2020-04-20 19:53:56 +0200

    missing qoutes around webpack argument

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 const path = require('path')
-const webpack = require(webpack)
+const webpack = require('webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+process.env.NODE_ENV = process.env.gitNODE_ENV || 'development'
 
 if (process.env.NODE_ENV === 'test') {
   require('dotenv').config({ path: '.env.test' })
