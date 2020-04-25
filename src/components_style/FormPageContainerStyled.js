12f088d54f13e8a4e84afb4edb38c9c9cec1774f
devByBjorn <devbybjorn@gmail.com>
2020-04-25 17:56:41 +0200

    tableSearchField

---
+++ src/components_style/FormPageContainerStyled.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const FormPageContainer = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+min-height: 90vh;
+`
+
+export default FormPageContainer
