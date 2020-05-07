0a5b582fe7664b0466cfedee8709f71efc3b50da
devByBjorn <devbybjorn@gmail.com>
2020-05-07 09:58:13 +0200

    spelling

--- src/components/EdgePusherPage.js
+++ src/components/EdgePusherPage.js
@@ -11,9 +11,9 @@ const EdgePusherPage = () => (
     <FlexContainer alignItems="center" flexDirection="column" justifyContent="center">
       <ContentWrapper>
         <SubHeading fontSize="5rem">Welcome<Span color="#f50057" display="inline">.</Span></SubHeading>
-        <Paragraph ><Span display="inline" fontWeight="bold">The market is a tuff place for a trader just starting out</Span>. One of the biggest issues for many new trades is undercapitalization. A small account size can make it hard to take the game serious, and lack of seriousness can easily lead you to blidly take too much risk. This often leads to another blown trading account.
+        <Paragraph ><Span display="inline" fontWeight="bold">The market is a tuff place for a trader just starting out</Span>. One of the biggest issues for many new trades is undercapitalization. A small account size can make it hard to take the game serious, and lack of seriousness can easily lead you to blindly take too much risk. This often leads to another blown trading account.
         </Paragraph>
-        <Paragraph>This can be avoided if you follow the process presented below. The process is especially suitable if you are trying to grow a small amount of money and do not have the luxury of risking only 0.5% - 1%, or even 2%, of your account. The two keystones of the process is; logging trades and comparing stats (a great deal of patience is needed too).</Paragraph>
+        <Paragraph>This can be avoided if you follow the process presented below. The process is especially suitable if you are trying to grow a small amount of money and do not have the luxury of risking only 0.5% - 1%, or even 2%, of your account. The two keystones of the process is logging trades and comparing stats (a great deal of patience is needed too).</Paragraph>
       </ContentWrapper>
 
       <ContentWrapper>
@@ -22,7 +22,7 @@ const EdgePusherPage = () => (
           The Trades page lets you log your trades and displays all your logged trades in form of tables. As of now you can log three types of trades; Closed, Active and Pending. Closed trades are just that, trades that have been executed and closed. Active trades have been executed but not yet closed. Pending trades are usually limit orders still waiting to get hit.
         </Paragraph>
 
-        <SubHeading><Span color="#f50057" display="inline">Trades</Span></SubHeading>
+        <SubHeading><Span color="#f50057" display="inline">Stats</Span></SubHeading>
         <Paragraph >
           Under the Stats page you have two lists; one with stats based on total trades, such as wins, losses ad win ratio, and another with stats based on R, such as total, highest, lowest and average R.
         </Paragraph>
@@ -30,7 +30,7 @@ const EdgePusherPage = () => (
           You will also find two tables, and here is where the magic lies. One table shows specific stats based on setups and the other shows the same stats based on markets. By looking at the tables, side by side, you can easily see which setups that are delivering your highest win rate and generates highest R, and which market that is your best performing one.
     </Paragraph>
         <Paragraph>
-          With this knowledge you should focus on only trading your highest probability setups in your best performing market, and really push those trades. Rember that you will need a good amount of trades before you can draw any conclusions based on the statistics.
+          With this knowledge you should focus on only trading your highest probability setups in your best performing market, and really push those trades. Remember that you will need a good amount of trades before you can draw any conclusions based on the statistics.
     </Paragraph>
       </ContentWrapper>
 
